ea62063f3d3ccc348573b678b18ef1687690f34c
fortnum <fortnum@mail.ru>
2022-11-17 15:10:16 +0000

    Initial commit

---
+++ main.cs
@@ -0,0 +1,8 @@
+using System;
+
+class Program { 
+  public static void Main (string[] args) {
+    Console.WriteLine("Hello Viktor");
+    Console.WriteLine ("Hello World. Args.Length={0}", args.Length);
+  }
+}
